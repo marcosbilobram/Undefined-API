f1e23372168f7739b68e7e0d5052fc42ad43f7e6
Marcos Bilobram <marcosbilobram@hotmail.com>
2023-05-16 21:47:02 -0300

    Base packages

---
+++ src/main/java/br/com/undefined/api/UndefinedApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.undefined.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndefinedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndefinedApiApplication.class, args);
+	}
+
+}
